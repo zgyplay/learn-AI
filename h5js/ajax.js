f5f94a16d594283ee8641c6067dabf7392033505
zgy <g55213642@163.com>
2017-09-12 16:19:26 +0800

    test promise

--- h5js/ajax.js
+++ h5js/ajax.js
@@ -28,3 +28,29 @@ function loadXMLDoc()
     xmlhttpReq.open("GET","/learn-AI/h5js/ajax.txt",true);
     xmlhttpReq.send();
 }
+
+// 测试 Promise
+function randFunc() {
+    var timeOut = Math.random() * 2;
+    console.log('set time out:' + timeOut + ' second.');
+    setTimeout(function (){
+        if(timeOut < 1){
+            console.log('call resolve...');
+            resolve('200 OK');
+        }
+        else {
+            console.log('call reject...');
+            console.log('timeout in' + timeOut + ' second');
+        }
+    }, timeOut * 1000);
+}
+
+function testPromise() {
+    new Promise(randFunc).then( function(result) {
+            console.log('成功' + result);
+        }
+    ).catch( function(reason) {
+            console.log('失败' + reason);
+        }
+    );
+}
