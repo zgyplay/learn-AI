c64d374bc46454928762bedf72ea8f43d26b2fa6
zgy <zgy.tienon@gmail.com>
2017-09-15 00:49:02 +0800

    did canvas 2D test

--- h5js/ajax.js
+++ h5js/ajax.js
@@ -56,3 +56,27 @@ function testPromise() {
         }
     );
 }
+
+//测试Canvas
+function testCanvas(){
+    let canv = document.getElementById('canvas1');
+    let ctx = canv.getContext('2d');//拿到一个CanvasRenderingContext2D对象
+    //WebGL规范，允许在Canvas中绘制3D图形
+    //let gl = canvas.getContext("webgl");
+
+    ctx.clearRect(0,0,200,200); //擦除(0,0)位置大小为200x200的矩形，擦除的意思是把该区域变为透明
+    ctx.fillStyle = '#dddddd'; // 设置颜色
+    ctx.fillRect(10, 10, 130, 130);// 把(10,10)位置大小为130x130的矩形涂色
+
+    // 利用Path绘制复杂路径:
+    var path=new Path2D();
+    path.arc(75, 75, 50, 0, Math.PI*2, true);
+    path.moveTo(110,75);
+    path.arc(75, 75, 35, 0, Math.PI, false);
+    path.moveTo(65, 65);
+    path.arc(60, 65, 5, 0, Math.PI*2, true);
+    path.moveTo(95, 65);
+    path.arc(90, 65, 5, 0, Math.PI*2, true);
+    ctx.strokeStyle = '#0000ff';
+    ctx.stroke(path);
+}
